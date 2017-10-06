e1be053bb6618cc901f4b470b17412411a0cabe8
lilpumpp <nrjazanskis@gmail.com>
2017-10-06 14:06:40 +0300

    erer

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 require 'vendor/autoload.php';
-echo 'nik_av';
-$app =new \atk4\ui\App('Registration');
+echo 'nik';
+$app =new \atk4\ui\App('Shops');
 $app->initLayout('Centered');
-$button1 = $app->add('Button');
+$button1 = $app->add('Signal');
 $button1->set('Apple');
 $button1->addClass("red");
 $button1->link('https://www.apple.com');
-$button2 = $app->add('Button');
+$button2 = $app->add('Browser');
 $button2->set('Samsung');
 $button2->addClass("yellow");
 $button2->link('https://www.samsung.com');
-$button = $app->add('Button');
+$button = $app->add('Feed');
 $button->set('Asus');
 $button->addClass("green");
 $button->link('https://www.asus.com');
