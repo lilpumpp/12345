e64e5cc277d0f982af07500925d12a1d81cf8949
lilpumpp <nrjazanskis@gmail.com>
2017-10-13 16:00:07 +0300

    a

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ $button1->addClass("red");
 $button1->link('https://www.apple.com');
 $button->icon = "Signal";
 $button2 = $app->add('Button');
+$button2->icon = "signal";
 $button2->set('Samsung');
 $button2->addClass("yellow");
 $button2->link('https://www.samsung.com');
