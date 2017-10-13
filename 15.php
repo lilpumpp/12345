9cc41e6706ff2903026082ad07dab2ed22da2642
lilpumpp <nrjazanskis@gmail.com>
2017-10-13 15:46:39 +0300

    s

--- 15.php
+++ 15.php
@@ -3,6 +3,7 @@ require 'vendor/autoload.php';
 echo 'nik';
 $app =new \atk4\ui\App('Shops');
 $app->initLayout('Centered');
-$label=$app->add('Label');
-$label->set('BOOM');
-$label->addClass('big purple');
+$label1=$app->add('Label');
+$label1->set('BOOM');
+$label1->addClass('big purple');
+$label= $app->add(['Label','Boom','detail'=>'hi'])
