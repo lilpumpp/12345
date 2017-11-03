89caeea7edc10abdcaf76b529922879fa2a5ee9b
lilpumpp <nrjazanskis@gmail.com>
2017-11-03 15:45:44 +0200

    i

--- test.php
+++ test.php
@@ -3,5 +3,7 @@ require 'vendor/autoload.php';
 $app =new \atk4\ui\App('Shops');
 $app->initLayout('Centered');
 
-$C=$_GET['name','surname'];
+$C=$_GET['name'];
+$label1=$app->add(['Label',$C]);
+$C=$_GET['surname'];
 $label1=$app->add(['Label',$C]);
