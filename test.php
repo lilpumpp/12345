9d70b832b1c9d59fb28c6f5fcb4515f152c33ee0
lilpumpp <nrjazanskis@gmail.com>
2017-11-10 16:03:31 +0200

    оощш

--- test.php
+++ test.php
@@ -5,4 +5,15 @@ $app->initLayout('Centered');
 $min=$_GET['min'];
 $max=$_GET['max'];
 $mid=round(($max+$min)/2);
-$label=$app->add(['Label','Твое число '.$mid.' ?']) 
+$label=$app->add(['Label','Твое число '.$mid.' ?']);
+
+$button=$app->add('Button');
+$button->set('Нет,не мое,мое больше');
+
+$button=$app->add('Button');
+$button->set('Нет,не мое,мое меньше');
+
+$button=$app->add('Button');
+$button->set('Да,мое');
+
+$button->link(['test','min'=>1,'max'=>100]);
